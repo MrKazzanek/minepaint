96761aae3328051a28a5d9900459f935b19e4397
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-25 20:21:40 +0100

    Update docs-config.js

--- docs-config.js
+++ docs-config.js
@@ -35,7 +35,7 @@ const docsConfig = {
                 category: "Navigating the Program",
                 pages: [
                     { title: "Drawing", id: "drawing-old", file: "pages/Alp-Y25R02/drawing.html" },
-                    { title: "Erasing", id: "erasing-old", file: "pages/Alp-Y25R01/erasing.html" },
+                    { title: "Erasing", id: "erasing-old", file: "pages/Alp-Y25R02/erasing.html" },
                     { title: "Quick block selection", id: "fast-pick-block-old", file: "pages/Alp-Y25R01/fast_pick_block.html" },
                     { title: "Draw block selection", id: "draw-block-selection-old", file: "pages/Alp-Y25R01/draw_block_selection.html" },
                     { title: "Saving & Loading", id: "save-and-load-old", file: "pages/Alp-Y25R01/save_and_load.html" }
@@ -48,19 +48,19 @@ const docsConfig = {
             {
                 category: "Getting Started",
                 pages: [
-                    { title: "Introduction", id: "intro-oldest", file: "pages/Alp-Y25R01/intro.html" },
+                    { title: "Introduction", id: "intro-old", file: "pages/Alp-Y25R01/intro.html" },
                     { title: "Authors", id: "authors", file: "pages/Alp-Y25R01/authors.html" },
-                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R01/install.html" }
+                    { title: "Installation", id: "install-oldest", file: "pages/Alp-Y25R01/install.html" }
                 ]
             },
             {
                 category: "Navigating the Program",
                 pages: [
-                    { title: "Drawing", id: "drawing-old", file: "pages/Alp-Y25R01/drawing.html" },
-                    { title: "Erasing", id: "erasing-old", file: "pages/Alp-Y25R01/erasing.html" },
-                    { title: "Quick block selection", id: "fast-pick-block-old", file: "pages/Alp-Y25R01/fast_pick_block.html" },
-                    { title: "Draw block selection", id: "draw-block-selection-old", file: "pages/Alp-Y25R01/draw_block_selection.html" },
-                    { title: "Saving & Loading", id: "save-and-load-old", file: "pages/Alp-Y25R01/save_and_load.html" }
+                    { title: "Drawing", id: "drawing-oldest", file: "pages/Alp-Y25R01/drawing.html" },
+                    { title: "Erasing", id: "erasing-oldest", file: "pages/Alp-Y25R01/erasing.html" },
+                    { title: "Quick block selection", id: "fast-pick-block-oldest", file: "pages/Alp-Y25R01/fast_pick_block.html" },
+                    { title: "Draw block selection", id: "draw-block-selection-oldest", file: "pages/Alp-Y25R01/draw_block_selection.html" },
+                    { title: "Saving & Loading", id: "save-and-load-oldest", file: "pages/Alp-Y25R01/save_and_load.html" }
                     
                 ]
             }
