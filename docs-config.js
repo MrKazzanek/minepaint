9b4cec782669f1ef42ea05a4d6cdbd9417b7977d
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-25 19:49:48 +0100

    Update docs-config.js

--- docs-config.js
+++ docs-config.js
@@ -26,7 +26,20 @@ const docsConfig = {
             {
                 category: "Getting Started",
                 pages: [
-                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R02/install.html" }
+                    { title: "Introduction", id: "intro-oldest", file: "pages/Alp-Y25R01/intro.html" },
+                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R02/install.html" },
+                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R01/install.html" }
+                ]
+            },
+            {
+                category: "Navigating the Program",
+                pages: [
+                    { title: "Drawing", id: "drawing-old", file: "pages/Alp-Y25R01/drawing.html" },
+                    { title: "Erasing", id: "erasing-old", file: "pages/Alp-Y25R01/erasing.html" },
+                    { title: "Quick block selection", id: "fast-pick-block-old", file: "pages/Alp-Y25R01/fast_pick_block.html" },
+                    { title: "Draw block selection", id: "draw-block-selection-old", file: "pages/Alp-Y25R01/draw_block_selection.html" },
+                    { title: "Saving & Loading", id: "save-and-load-old", file: "pages/Alp-Y25R01/save_and_load.html" }
+                    
                 ]
             }
         ],
