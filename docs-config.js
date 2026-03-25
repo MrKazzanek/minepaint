7a34c70efe4baad1a8a2367b45f81877ad95017c
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-25 18:24:55 +0100

    Update docs-config.js

--- docs-config.js
+++ docs-config.js
@@ -1,27 +1,27 @@
 const docsConfig = {
     // Domyślna (najnowsza) wersja
-    defaultVersion: "v1.3",
+    defaultVersion: "Alp-Y25R01",
     // Lista dostępnych wersji do wyboru w dropdownie
-    versions: ["v1.3", "Alp-Y25R01"],
+    versions: ["Alp-Y25R01"],
     
     // Struktura zawartości dla każdej wersji
     content: {
-        "v1.3": [
-            {
-                category: "Getting Started",
-                pages: [
-                    { title: "Introduction", id: "intro", file: "pages/v1.3/intro.html" },
-                    { title: "Installation", id: "install", file: "pages/v1.3/install.html" }
-                ]
-            },
-            {
-                category: "Tools & Interface",
-                pages: [
-                    { title: "Basic Tools", id: "basic-tools", file: "pages/v1.3/tools.html" },
-                    { title: "Layer System", id: "layers", file: "pages/v1.3/layers.html" }
-                ]
-            }
-        ],
+        // "v1.3": [
+        //     {
+        //         category: "Getting Started",
+        //         pages: [
+        //             { title: "Introduction", id: "intro", file: "pages/v1.3/intro.html" },
+        //             { title: "Installation", id: "install", file: "pages/v1.3/install.html" }
+        //         ]
+        //     },
+        //     {
+        //         category: "Tools & Interface",
+        //         pages: [
+        //             { title: "Basic Tools", id: "basic-tools", file: "pages/v1.3/tools.html" },
+        //             { title: "Layer System", id: "layers", file: "pages/v1.3/layers.html" }
+        //         ]
+        //     }
+        // ],
         "Alp-Y25R01": [
             {
                 category: "Getting Started",
@@ -30,7 +30,7 @@ const docsConfig = {
                     { title: "Installation", id: "install-old", file: "pages/Alp-Y25R01/install.html" }
                 ]
             }
-            // Mniej narzędzi, bo to starsza wersja
+            
         ]
     }
 };
