1e9478628af8229f552c49207f5901b138d98b4e
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-25 17:34:12 +0100

    Create docs-config.js

---
+++ docs-config.js
@@ -0,0 +1,36 @@
+const docsConfig = {
+    // Domyślna (najnowsza) wersja
+    defaultVersion: "v1.3",
+    // Lista dostępnych wersji do wyboru w dropdownie
+    versions: ["v1.3", "v1.2"],
+    
+    // Struktura zawartości dla każdej wersji
+    content: {
+        "v1.3": [
+            {
+                category: "Getting Started",
+                pages: [
+                    { title: "Introduction", id: "intro", file: "pages/v1.3/intro.html" },
+                    { title: "Installation", id: "install", file: "pages/v1.3/install.html" }
+                ]
+            },
+            {
+                category: "Tools & Interface",
+                pages: [
+                    { title: "Basic Tools", id: "basic-tools", file: "pages/v1.3/tools.html" },
+                    { title: "Layer System", id: "layers", file: "pages/v1.3/layers.html" }
+                ]
+            }
+        ],
+        "v1.2": [
+            {
+                category: "Getting Started",
+                pages: [
+                    { title: "Introduction", id: "intro-old", file: "pages/v1.2/intro.html" },
+                    { title: "Installation", id: "install-old", file: "pages/v1.2/install.html" }
+                ]
+            }
+            // Mniej narzędzi, bo to starsza wersja
+        ]
+    }
+};
