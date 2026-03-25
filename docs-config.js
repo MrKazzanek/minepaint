721c7c3391f7810d83c7d9fcaa75d0ed184cf55c
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-25 19:50:52 +0100

    Update docs-config.js

--- docs-config.js
+++ docs-config.js
@@ -27,8 +27,8 @@ const docsConfig = {
                 category: "Getting Started",
                 pages: [
                     { title: "Introduction", id: "intro-oldest", file: "pages/Alp-Y25R01/intro.html" },
-                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R02/install.html" },
-                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R01/install.html" }
+                    { title: "Authors", id: "authors", file: "pages/Alp-Y25R01/authors.html" }
+                    { title: "Installation", id: "install-old", file: "pages/Alp-Y25R02/install.html" }
                 ]
             },
             {
