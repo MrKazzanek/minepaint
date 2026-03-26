9aa63f2b201d9bb13314f42a60fd137641b0d12b
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-26 17:04:18 +0100

    Update docs-config.js

--- docs-config.js
+++ docs-config.js
@@ -1,8 +1,8 @@
 const docsConfig = {
     // Domyślna (najnowsza) wersja
-    defaultVersion: "Alp-Y25R02",
+    defaultVersion: "Alp-Y26R01",
     // Lista dostępnych wersji do wyboru w dropdownie
-    versions: ["Alp-Y25R02","Alp-Y25R01"],
+    versions: ["Alp-Y26R01","Alp-Y25R02","Alp-Y25R01"],
     
     // Struktura zawartości dla każdej wersji
     content: {
@@ -22,6 +22,36 @@ const docsConfig = {
         //         ]
         //     }
         // ],
+        "Alp-Y26R01": [
+            {
+                category: "Getting Started",
+                pages: [
+                    { title: "Introduction", id: "intro-oldest", file: "pages/Alp-Y25R01/intro.html" },
+                    { title: "Authors", id: "authors", file: "pages/Alp-Y25R01/authors.html" },
+                    { title: "Installation", id: "install-1", file: "pages/Alp-Y26R01/install.html" }
+                ]
+            },
+            {
+                category: "Navigating the Program",
+                pages: [
+                    { title: "Tool Selection", id: "tool-select-old", file: "pages/Alp-Y25R02/tool.html" },
+                    { title: "Drawing", id: "drawing-old", file: "pages/Alp-Y25R02/drawing.html" },
+                    { title: "Filling", id: "filling-old", file: "pages/Alp-Y25R02/filling.html" },
+                    { title: "Erasing", id: "erasing-old", file: "pages/Alp-Y25R02/erasing.html" },
+                    { title: "Quick block selection", id: "fast-pick-block-old", file: "pages/Alp-Y25R02/fast_pick_block.html" },
+                    { title: "Inventory", id: "draw-block-selection-old", file: "pages/Alp-Y25R02/draw_block_selection.html" },
+                    { title: "Blockstates Inventory", id: "blockstates-inventory-old", file: "pages/Alp-Y25R02/blockstates.html" },
+                    { title: "Hotbar", id: "hotbar-old", file: "pages/Alp-Y25R02/hotbar.html" },
+                    { title: "Saving & Loading", id: "save-and-load-old", file: "pages/Alp-Y25R01/save_and_load.html" },
+                    { title: "Resizing Canvas", id: "resize-old", file: "pages/Alp-Y25R02/resize.html" },
+                    { title: "Layers System", id: "layers-old", file: "pages/Alp-Y25R02/layers.html" },
+                    { title: "Keyboard Shortcuts", id: "keyboard-shortcuts-old", file: "pages/Alp-Y25R02/shortcuts.html" }
+                    
+                    
+                ]
+            }
+        ],
+        //odzielacz
         "Alp-Y25R02": [
             {
                 category: "Getting Started",
