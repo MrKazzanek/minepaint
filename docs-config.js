fcbb6e6a2d2cbad5e17778d1eaf1f7b7f1ff4e3c
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-03-28 10:59:32 +0100

    Update docs-config.js

--- docs-config.js
+++ docs-config.js
@@ -37,6 +37,7 @@ const docsConfig = {
                     { title: "Tool Selection", id: "tool-select-old", file: "pages/Alp-Y25R02/tool.html" },
                     { title: "Drawing", id: "drawing-old", file: "pages/Alp-Y25R02/drawing.html" },
                     { title: "Brush Size", id: "pen-size-old", file: "pages/Alp-Y26R01/tool_size.html" },
+                    { title: "Brush Shape", id: "brush-shape-old", file: "pages/Alp-Y26R02/brush_shape.html" },
                     { title: "Filling", id: "filling-old", file: "pages/Alp-Y25R02/filling.html" },
                     { title: "Erasing", id: "erasing-old", file: "pages/Alp-Y25R02/erasing.html" },
                     { title: "Quick block selection", id: "fast-pick-block-old", file: "pages/Alp-Y25R02/fast_pick_block.html" },
